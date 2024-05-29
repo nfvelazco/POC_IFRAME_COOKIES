9bace98fb65b8a60f0847cd6c10dafe0b367f5fe
Nicolas Velazco <nfvelazco@gmail.com>
2024-05-29 16:23:03 -0300

    Se fixeó el valor por defecto de la cookie una vez renderizado el principal

--- scripts/main-principal.js
+++ scripts/main-principal.js
@@ -6,21 +6,17 @@ var json_valor_por_defecto = {flag_novedad: false,id_producto: -1};
 // Convertir el objeto a una cadena JSON
 var valorCookie = JSON.stringify(json_valor_por_defecto);
 Cookies.set(NAME_COOKIE, valorCookie); // Crea una cookie de sesión
-//Cookies.set('nombre', 'valor', { expires: 7 }); // Crea una cookie que expira en 7 días
+
+const setCookieFinishRender = ()=>{
+    console.log(`Reestableciendo el valor por defecto de la coockie de session`)
+    Cookies.set(NAME_COOKIE, valorCookie);
+}
 
 const render_producto = (valor) =>{
     $("#txtProducto").html(`Producto id: ${valor.id_producto}`)
 }
 
 $(document).ready(()=>{
-    
-    
-    var miCookie = Cookies.get(NAME_COOKIE); // Obtiene el valor de la cookie
-    var valor = miCookie ? JSON.parse(miCookie) : undefined
-    console.log(valor)
-    if((valor != undefined)&&(valor.flag_novedad)){
-        console.log(valor.id_producto)
-    }
 
     var checkCookie = setInterval(function() {
         var miCookie = Cookies.get(NAME_COOKIE); // Obtiene el valor de la cookie
@@ -28,6 +24,8 @@ $(document).ready(()=>{
         if((valor != undefined)&&(valor.flag_novedad)){
             // Ejecuta tu función aquí
             render_producto(valor);
+            console.log(`El valor fue actualizado a: ${valor.id_producto}`)
+            setCookieFinishRender();  
         } else {
             // Si la cookie no es 'false', detiene la iteración
             //clearInterval(checkCookie);
